8f27cfe8c57ca69a7406b540e8a6e38c2d1a7cd5
Lon <gavinlondee@gmail.com>
2019-12-13 13:34:25 +0200

    Initial commit

---
+++ BasicCalculator/BlankUserInputException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BasicCalculator
+{
+    [Serializable]
+    public class BlankUserInputException : Exception
+    {
+
+        public BlankUserInputException()
+        { 
+        }
+
+        public BlankUserInputException(string message):base(message)
+        { 
+        }
+
+        public BlankUserInputException(string message, Exception innerException):base(message, innerException)
+        { 
+        }
+
+        public BlankUserInputException(SerializationInfo info, StreamingContext context):base(info,context)
+        { 
+        
+        }
+    }
+}
